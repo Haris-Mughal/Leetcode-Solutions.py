75dbfa22d5ed1e6c299a3422746746f50ab4c89b
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-12-25 01:31:20 +0500

    Time: 6 ms (64.92%) | Memory: 17.9 MB (12.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -3,18 +3,20 @@ class Solution:
         if len(s) != len(t):
             return False
 
+    # Create dictionaries to store character mappings
         map_s_to_t = {}
         map_t_to_s = {}
 
         for char_s, char_t in zip(s, t):
-            
+        # Check if mappings are consistent
             if char_s in map_s_to_t:
                 if map_s_to_t[char_s] != char_t:
                     return False
             if char_t in map_t_to_s:
                 if map_t_to_s[char_t] != char_s:
                     return False
-    
+        
+        # Add the mappings
             map_s_to_t[char_s] = char_t
             map_t_to_s[char_t] = char_s
 
