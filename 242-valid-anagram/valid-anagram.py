1e4e7faa184d385dfe9e68d7ed5c90cee76d1634
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2025-04-20 20:27:18 +0500

    Time: 11 ms (72.40%) | Memory: 17.7 MB (97.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,39 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        # return sorted(s) == sorted(t)
-
-        # ---
-        # if len(s) != len(t):
-        #     return False
-
-        # count_s = {}
-        # count_t = {}
-
-        # for char in s:
-        #     count_s[char] = count_s.get(char, 0) + 1
-
-        # for char in t:
-        #     count_t[char] = count_t.get(char, 0) + 1
-
-        # return count_s == count_t
-
-        # ---
         if len(s) != len(t):
             return False
 
-        stack = list(s)
-        queue = deque(t)
+        count = [0] * 26
 
-        count_s = {}
-        count_t = {}
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
 
-        while stack:
-            ch = stack.pop()
-            count_s[ch] = count_s.get(ch, 0) + 1
-
-        while queue:
-            ch = queue.popleft()
-            count_t[ch] = count_t.get(ch, 0) + 1
-
-        return count_s == count_t
+        return all(c == 0 for c in count)
