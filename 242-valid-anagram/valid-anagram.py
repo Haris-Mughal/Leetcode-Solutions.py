4cdf535faaf45691147d0767dd770603a8f1df47
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2025-04-21 01:24:41 +0500

    Time: 11 ms (72.47%) | Memory: 18.1 MB (33.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,9 +6,9 @@ class Solution:
 
         count = [0] * 26
 
-        for i in range(len(s)):
-            count[ord(s[i]) - ord("a")] += 1
-            count[ord(t[i]) - ord("a")] -= 1
+        for i, j in zip(s, t):
+            count[ord(i) - ord("a")] += 1
+            count[ord(j) - ord("a")] -= 1
 
         return all(c == 0 for c in count)
 
