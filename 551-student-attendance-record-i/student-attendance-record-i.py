bc92796cfcab4a856cd56451420318cff4dccc29
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2025-04-23 01:18:18 +0500

    Time: 3 ms (8.43%) | Memory: 17.7 MB (84.84%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.py
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -1,11 +1,12 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
+
         a = 0
 
         for i in range(len(s)):
             if s[i] == "A":
                 a += 1
-                
+
             elif s[i:i+3] == "LLL":
                 return False
         
