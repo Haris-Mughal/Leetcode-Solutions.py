af0e2a52fe91f52556eb523e21368f68859a7ca6
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2025-04-24 20:02:28 +0500

    Time: 32 ms (61.34%) | Memory: 34.7 MB (92.81%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -6,11 +6,38 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
 
-        curr = head
-        listVal = []
+        # curr = head
+        # listVal = []
 
-        while curr:
-            listVal.append(curr.val)
-            curr = curr.next
+        # while curr:
+        #     listVal.append(curr.val)
+        #     curr = curr.next
 
-        return listVal == listVal[::-1]
+        # return listVal == listVal[::-1]
+        
+        if not head or not head.next:
+            return True
+
+        # Step 1: Find the middle of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # Step 3: Compare both halves
+        left, right = head, prev
+        while right:  # right half might be shorter in odd-length list
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
