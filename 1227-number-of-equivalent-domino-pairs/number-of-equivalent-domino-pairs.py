0883a1b3d5435d73fe892ee883fa478b2e134023
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2025-05-06 21:52:38 +0500

    Time: 18 ms (12.99%) | Memory: 24.3 MB (42.05%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.py
@@ -1,11 +1,15 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        count = [0] * 100  # keys go from 11 to 99
+        count = {}
         result = 0
 
         for a, b in dominoes:
-            key = 10 * min(a, b) + max(a, b)
-            result += count[key]
-            count[key] += 1
+            key = (min(a, b), max(a, b))
+        
+            if key in count:
+                result += count[key]
+                count[key] += 1
+            else:
+                count[key] = 1
 
         return result
